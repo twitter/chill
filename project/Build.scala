0920dc878883a89e805f3fd82d1c36dde23ece2e
Oscar Boykin <oscar@twitter.com>
2013-04-19 17:37:23 -0700

    version bump 0.2.1

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object ChillBuild extends Build {
   val sharedSettings = Project.defaultSettings ++
     mimaDefaultSettings ++ Seq(
 
-  version := "0.2.1-SNAPSHOT",
+  version := "0.2.1",
 
   organization := "com.twitter",
 
