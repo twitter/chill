77b8a73d7643263249f76ae03c0d3f03db330fc3
Ian O Connell <ioconnell@twitter.com>
2015-08-16 18:14:04 -0700

    Adds two Injection's to lift T into an Externalizer of T, and codec for Externalizer[T]

---
+++ chill-bijection/src/main/scala/com/twitter/chill/ExternalizerCodec.scala
@@ -0,0 +1,48 @@
+/*
+Copyright 2012 Twitter, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.twitter.chill
+
+import _root_.java.io.{
+  ByteArrayOutputStream,
+  ByteArrayInputStream,
+  ObjectInputStream,
+  ObjectOutputStream
+}
+
+import com.twitter.bijection.Injection
+import scala.util.Try
+
+/**
+ * A Injection to serialize an externalizer to bytes
+ */
+object ExternalizerCodec {
+  implicit def apply[T]: ExternalizerCodec[T] = new ExternalizerCodec[T]
+}
+
+class ExternalizerCodec[T] extends Injection[Externalizer[T], Array[Byte]] {
+  def apply(extern: Externalizer[T]): Array[Byte] = {
+    val baos = new ByteArrayOutputStream()
+    val oos = new ObjectOutputStream(baos)
+    oos.writeObject(extern)
+    baos.toByteArray
+  }
+  def invert(bytes: Array[Byte]): Try[Externalizer[T]] = Try {
+    val testInput = new ByteArrayInputStream(bytes)
+    val ois = new ObjectInputStream(testInput)
+    ois.readObject.asInstanceOf[Externalizer[T]] // this may throw
+  }
+}
---
+++ chill-bijection/src/main/scala/com/twitter/chill/ExternalizerInjection.scala
@@ -0,0 +1,43 @@
+/*
+Copyright 2012 Twitter, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.twitter.chill
+
+import _root_.java.io.{
+  ByteArrayOutputStream,
+  ByteArrayInputStream,
+  ObjectInputStream,
+  ObjectOutputStream
+}
+
+import com.twitter.bijection.Injection
+import scala.util.Try
+
+/**
+ * An Injection to lift a type T into an Externalizer
+ */
+object ExternalizerInjection {
+  implicit def apply[T]: ExternalizerInjection[T] = new ExternalizerInjection[T]
+}
+
+class ExternalizerInjection[T] extends Injection[T, Externalizer[T]] {
+  def apply(t: T): Externalizer[T] = {
+    Externalizer(t)
+  }
+  def invert(extern: Externalizer[T]): Try[T] = Try {
+    extern.get
+  }
+}
