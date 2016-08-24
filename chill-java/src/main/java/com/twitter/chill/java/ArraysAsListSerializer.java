3f00002a21b6b0affe4dda69dcc90dc5bcb18a6d
Alex Chermenin <a.chermenin@gmail.com>
2016-08-24 17:18:35 +0300

    Fix primitive types.

--- chill-java/src/main/java/com/twitter/chill/java/ArraysAsListSerializer.java
+++ chill-java/src/main/java/com/twitter/chill/java/ArraysAsListSerializer.java
@@ -58,34 +58,57 @@ public class ArraysAsListSerializer extends Serializer<List<?>> {
     @Override
     public List<?> read(final Kryo kryo, final Input input, final Class<List<?>> type) {
         final int length = input.readInt(true);
-        final Class<?> componentType = kryo.readClass( input ).getType();
+        Class<?> componentType = kryo.readClass(input).getType();
         try {
-            final Object items = Array.newInstance(componentType, length);
+            final Object items = Array.newInstance(getBoxedClass(componentType), length);
             for (int i = 0; i < length; i++) {
                 Array.set(items, i, kryo.readClassAndObject(input));
             }
-            return Arrays.asList(items);
-        } catch ( final Exception e ) {
-            throw new RuntimeException( e );
+            return Arrays.asList((Object[]) items);
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     @Override
     public void write(final Kryo kryo, final Output output, final List<?> obj) {
         try {
-            final Object[] array = (Object[]) _arrayField.get( obj );
+            final Object[] array = (Object[]) _arrayField.get(obj);
             output.writeInt(array.length, true);
             final Class<?> componentType = array.getClass().getComponentType();
-            kryo.writeClass( output, componentType );
-            for( final Object item : array ) {
-                kryo.writeClassAndObject( output, item );
+            kryo.writeClass(output, componentType);
+            for (final Object item : array) {
+                kryo.writeClassAndObject(output, item);
             }
-        } catch ( final RuntimeException e ) {
-             // Don't eat and wrap RuntimeExceptions because the ObjectBuffer.write...
-             // handles SerializationException specifically (resizing the buffer)...
-             throw e;
-        } catch ( final Exception e ) {
-             throw new RuntimeException( e );
+        } catch (final RuntimeException e) {
+            // Don't eat and wrap RuntimeExceptions because the ObjectBuffer.write...
+            // handles SerializationException specifically (resizing the buffer)...
+            throw e;
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
         }
+    }
+    
+    private static Class<?> getBoxedClass(final Class<?> c) {
+        if (c.isPrimitive()) {
+            if (c.equals(Long.TYPE)) {
+                return Long.class;
+            } else if (c.equals(Integer.TYPE)) {
+                return Integer.class;
+            } else if (c.equals(Short.TYPE)) {
+                return Short.class;
+            } else if (c.equals(Byte.TYPE)) {
+                return Byte.class;
+            } else if (c.equals(Character.TYPE)) {
+                return Character.class;
+            } else if (c.equals(Double.TYPE)) {
+                return Double.class;
+            } else if (c.equals(Float.TYPE)) {
+                return Float.class;
+            } else if (c.equals(Boolean.TYPE)) {
+                return Boolean.class;
+            }
+        }
+        return c;
     }
 }
