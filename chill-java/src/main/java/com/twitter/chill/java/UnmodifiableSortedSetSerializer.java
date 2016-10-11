173f4851dbfc86132f70170ff7ea8902105281ab
Alex Chermenin <a.chermenin@gmail.com>
2016-10-11 03:22:02 +0300

    Serializers for unmodifiable Java collections. (#260)
    * Serializers for unmodifiable and empty collections.
    
    Added serializers for collections created via
    Collections.unmodifiable...() and Collections.empty...() methods.
    
    * Some refactoring.
    
    Serializers of unmodifiable Java collections was refactored.
    Deleted serializers for empty collections (Kryo has default serializators for it).

---
+++ chill-java/src/main/java/com/twitter/chill/java/UnmodifiableSortedSetSerializer.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016 Alex Chermenin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.twitter.chill.java;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.twitter.chill.IKryoRegistrar;
+import com.twitter.chill.SingleRegistrar;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+/**
+ * A kryo {@link Serializer} for unmodifiable sortable sets created via {@link Collections#unmodifiableSortedSet(SortedSet)}.
+ * <p>
+ * Note: This serializer does not support cyclic references, so if one of the objects
+ * gets set the list as attribute this might cause an error during deserialization.
+ * </p>
+ *
+ * @author <a href="mailto:alex@chermenin.ru">Alex Chermenin</a>
+ */
+public class UnmodifiableSortedSetSerializer extends UnmodifiableJavaCollectionSerializer<SortedSet<?>> {
+  
+  @SuppressWarnings("unchecked")
+  static public IKryoRegistrar registrar() {
+    return new SingleRegistrar(Collections.unmodifiableSortedSet(new TreeSet(Collections.EMPTY_SET)).getClass(),
+        new UnmodifiableSortedSetSerializer());
+  }
+  
+  @Override
+  protected Field getInnerField() throws Exception {
+    return Class.forName("java.util.Collections$UnmodifiableSortedSet").getDeclaredField("ss");
+  }
+  
+  @Override
+  protected SortedSet<?> newInstance(SortedSet<?> s) {
+    return Collections.unmodifiableSortedSet(s);
+  }
+}
