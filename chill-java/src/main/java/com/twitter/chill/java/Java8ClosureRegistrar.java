b2ec04e6522eaa0babc4c34a865be1af627e77f0
Josh Rosen <joshrosen@databricks.com>
2016-11-07 09:40:19 -0800

    Fix Java8ClosureRegistrar; standardize formatting.

--- chill-java/src/main/java/com/twitter/chill/java/Java8ClosureRegistrar.java
+++ chill-java/src/main/java/com/twitter/chill/java/Java8ClosureRegistrar.java
@@ -8,13 +8,15 @@ import com.twitter.chill.IKryoRegistrar;
  * Enables Java 8 lambda serialization if running on Java 8; no-op otherwise.
  */
 public class Java8ClosureRegistrar implements IKryoRegistrar {
-  @Override
-  public void apply(Kryo k) {
-    try {
-      Class.forName("java.lang.invoke.SerializedLambda");
-    } catch (ClassNotFoundException e) {
-      return;
+
+    @Override
+    public void apply(Kryo k) {
+        try {
+            Class.forName("java.lang.invoke.SerializedLambda");
+        } catch (ClassNotFoundException e) {
+            // Not running on Java 8.
+            return;
+        }
+        k.register(ClosureSerializer.Closure.class, new ClosureSerializer());
     }
-    k.register(ClosureSerializer.class, new ClosureSerializer());
-  }
 }
