1bb412d8f322f6ac533236bb67038677db96dab1
Georg <gdiet@users.noreply.github.com>
2018-01-23 15:32:58 +0100

    found a problem when serializing streams

--- chill-scala/src/test/scala/com/twitter/chill/StandardDataRegistrationsSpec.scala
+++ chill-scala/src/test/scala/com/twitter/chill/StandardDataRegistrationsSpec.scala
@@ -43,40 +43,54 @@ class StandardDataRegistrationsSpec extends WordSpec with Matchers {
             assert(false, message)
         }
       }
+      def tuples(count: Int): Seq[(Int, Int)] = Seq.range(0, count).map(n => (n, n + 1))
       "serialize the empty map" in { roundtrip(Map()) }
       "serialize the one-element map" in { roundtrip(Map(1 -> 2)) }
+      "serialize larger maps" in { roundtrip(Map(tuples(2): _*), Map(tuples(3): _*), Map(tuples(4): _*), Map(tuples(5): _*)) }
       "serialize the empty hash map" in { roundtrip(HashMap()) }
       "serialize the one-element hash map" in { roundtrip(HashMap(1 -> 2)) }
-      // TODO larger hash maps
+      "serialize larger hash maps" in { roundtrip(HashMap(tuples(2): _*), HashMap(tuples(3): _*), HashMap(tuples(4): _*), HashMap(tuples(5): _*)) }
       "serialize the empty list map" in { roundtrip(ListMap()) }
       "serialize the one-element list map" in { roundtrip(ListMap(1 -> 2)) }
+      "serialize larger list maps" in { roundtrip(ListMap(tuples(2): _*), ListMap(tuples(3): _*), ListMap(tuples(4): _*), ListMap(tuples(5): _*)) }
       "serialize the empty tree map" in { roundtrip(TreeMap.empty[Int, Int]) }
       "serialize the one-element tree map" in { roundtrip(TreeMap(1 -> 2)) }
-      // TODO larger list maps
+      "serialize larger tree maps" in { roundtrip(TreeMap(tuples(2): _*), TreeMap(tuples(3): _*), TreeMap(tuples(4): _*), TreeMap(tuples(5): _*)) }
       "serialize the empty set" in { roundtrip(Set()) }
+      "serialize larger sets" in { roundtrip(Set(1), Set(1, 2), Set(1, 2, 3), Set(1, 2, 3, 4), Set(1, 2, 3, 4, 5)) }
       "serialize the empty hash set" in { roundtrip(HashSet()) }
       "serialize the one-element hash set" in { roundtrip(HashSet(1)) }
+      "serialize larger hash sets" in { roundtrip(HashSet(1, 2), HashSet(1, 2, 3), HashSet(1, 2, 3, 4), HashSet(1, 2, 3, 4, 5)) }
       "serialize the empty list set" in { roundtrip(ListSet()) }
       "serialize the one-element list set" in { roundtrip(ListSet(1)) }
+      "serialize larger list sets" in { roundtrip(ListSet(1, 2), ListSet(1, 2, 3), ListSet(1, 2, 3, 4), ListSet(1, 2, 3, 4, 5)) }
       "serialize the empty tree set" in { roundtrip(TreeSet.empty[Int]) }
       "serialize the one-element tree set" in { roundtrip(TreeSet(1)) }
-      // TODO more examples like Set(1)
+      "serialize larger tree sets" in { roundtrip(TreeSet(1, 2), TreeSet(1, 2, 3), TreeSet(1, 2, 3, 4), TreeSet(1, 2, 3, 4, 5)) }
       "serialize the empty list" in { roundtrip(Nil) }
       "serialize the one-element list" in { roundtrip(List(1)) }
-      // TODO more examples like List.empty[Int], 1 :: Nil, List(1, 2), List(1, 2, 3, 4)
+      "serialize alternative ways to instantiate lists" in { roundtrip(List.empty[Int], 1 :: Nil) }
+      "serialize larger lists" in { roundtrip(List(1, 2), List(1, 2, 3), List(1, 2, 3, 4), List(1, 2, 3, 4, 5)) }
       "serialize the empty queue" in { roundtrip(Queue()) }
-      // TODO more examples like Queue(1)
+      "serialize larger queues" in { roundtrip(Queue(1), Queue(1, 2), Queue(1, 2, 3), Queue(1, 2, 3, 4), Queue(1, 2, 3, 4, 5)) }
       "serialize a range" in { roundtrip(Range(2, 10, 3)) }
-      // TODO more examples like Vector(), Vector(1)
+      "serialize vectors" in { roundtrip(Vector(), Vector(1), Vector(1, 2), Vector(1, 2, 3), Vector(1, 2, 3, 4), Vector(1, 2, 3, 4, 5)) }
       "serialize the empty stream" in { roundtrip(Stream()) }
       "serialize the one-element stream" in { roundtrip(Stream(1)) }
-      "serialize the empty option" in { roundtrip(None) }
-      // TODO more examples like Some(1), Left(2), Right(3), Option.empty, Option(5)
+      "serialize larger streams2" in { roundtrip(Stream(1, 2)) } // FIXME remove 2,3,4
+      "serialize larger streams3" in { roundtrip(Stream(1, 2), Stream(1, 2, 3)) }
+      "serialize larger streams4" in { roundtrip(Stream(1, 2), Stream(1, 2, 3), Stream(1, 2, 3, 4)) }
+      "serialize larger streams" in { roundtrip(Stream(1, 2), Stream(1, 2, 3), Stream(1, 2, 3, 4), Stream(1, 2, 3, 4, 5)) }
+      "serialize the options" in { roundtrip(None, Some(1), Option.empty[Int], Option(3)) }
+      "serialize the eithers" in { roundtrip(Left(2), Right(4), Left.apply[Int, Int](3)) }
       "serialize the empty array" in { roundtrip(Array()) }
-      "serialize Int arrays" in { roundtrip(Array.empty[Int]) }
-      "serialize Byte arrays" in { roundtrip(Array.empty[Byte]) }
-      "serialize Short arrays" in { roundtrip(Array.empty[Short]) }
-      "serialize Long arrays" in { roundtrip(Array.empty[Long]) }
+      "serialize empty Int arrays" in { roundtrip(Array.empty[Int]) }
+      "serialize Int arrays" in { roundtrip(Array(4, 2)) }
+      "serialize empty Short arrays" in { roundtrip(Array.empty[Short]) }
+      "serialize Short arrays" in { roundtrip(Array(3.toShort, 4.toShort)) }
+      "serialize empty Byte arrays" in { roundtrip(Array.empty[Byte]) }
+      "serialize Byte arrays" in { roundtrip(Array(3.toByte, 4.toByte)) }
+      "serialize Long arrays" in { roundtrip(Array.empty[Long]) } // FIXME continue with adding more example
       "serialize Float arrays" in { roundtrip(Array.empty[Float]) }
       "serialize Double arrays" in { roundtrip(Array.empty[Double]) }
       "serialize Boolean arrays" in { roundtrip(Array.empty[Boolean]) }
